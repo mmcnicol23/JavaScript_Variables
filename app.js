3c597f37fb3dc590f23283203fd09b28f10cd6e9
Mariah McNicol <mariahmcnicol@gmail.com>
2024-09-25 21:57:30 -0400

    Complete JavaScript exercises 1-5

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Mariah";
+let lastName = "McNicol";
+var age = 32;
 
 // Exercise 2
+lastName = ""
+age = 52;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${selectedSize} is ${salePercentage} off at only ${price}`)
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`I am reading ${title} by ${author}. I am on page ${bookmark} of ${pageCount}`)
